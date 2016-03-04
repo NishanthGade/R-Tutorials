46b172afd3231eae368d2edde504a812f64e9441
NishanthGade <nishanth_gg@yahoo.com>
2016-03-04 07:13:28 -0800

    Added files via upload

---
+++ ChiSqTest.R
@@ -0,0 +1,12 @@
+#To test independence of events
+tb<-matrix(c(200,150,50,250,300,50),c(2,3),byrow=T)
+chisq.test(tb)
+
+#To test goodness of fit
+originalFrequencies<- c(21,109,62,15)
+expectedProportions<- c(0.08,0.47,0.34,0.11) #given
+expectedFrequencies<- c(16.56,97.29,70.38,22.77) #calculated based on proportions and observed 
+
+chisq.test(originalFrequencies, p = expectedFrequencies /sum(expectedFrequencies))
+#OR
+chisq.test(originalFrequencies, p = expectedProportions)
