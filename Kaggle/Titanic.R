d496b82b9fd87aac97e348494f8556897ac615e1
NishanthGade <nishanth_gg@yahoo.com>
2016-05-11 09:10:49 -0700

    Update Titanic.R

--- Kaggle/Titanic.R
+++ Kaggle/Titanic.R
@@ -80,10 +80,10 @@ trainset$PredElastic <- predict(elastic, tr.mat, type = "response")
 table(trainset$Survived, trainset$PredElastic>=0.6)
 
 ########################Decision Trees###############
-dat<-read.csv("E:\\Nishanth\\Kaggle\\Datasets\\Titanic\\train.csv")
-
+dat<-read.csv("D:\\Practice\\Titanic\\train.csv")
+#install.packages("KRLS")
 str(dat)
-dat<-dat[,-c(1,4,9,11)]
+dat<-dat[,-c(1,9)]
 dat$Survived<-as.factor(as.character(dat$Survived))
 dat$Pclass<-as.factor(as.character(dat$Pclass))
 dat$SibSp<-as.factor(as.character(dat$SibSp))
@@ -94,15 +94,40 @@ sum(is.na(dat))
 library(DMwR)
 dat1 <- centralImputation(dat)
 
-library(infotheo)
-dat1_Num_binned <- discretize(X=dat1[,c("Age","Fare")],disc="equalfreq", nbins=3)
-dat2 <- data.frame(dat1[,-c(4,7)], dat1_Num_binned)
+levels(dat1$Embarked)
+table(dat1$Embarked)[1]
 
-set.seed(24)
-rows<-seq(1,nrow(dat2),1)
-train<-sample(rows,nrow(dat2)*.7)
-trainset<-dat2[train,]
-testset<-dat2[-train,]
+levels(dat1$Cabin)
+table(dat1$Cabin)[1]
+
+levels(dat1$Embarked)[1] = "missing"
+levels(dat1$Cabin)[1] = "missing"
+
+library(stringr)
+cabin <- as.character(dat1$Cabin)
+cabin1 <- ifelse(cabin != "missing", substr(cabin,1,1), "M")
+cabin2 <- ifelse(cabin != "missing", as.numeric(substr(cabin,2,length(cabin))), 0)
+
+name <- as.character(dat1$Name)
+sum(name == "")
+salulation <- substr(name, str_locate(name, ", ")+2, str_locate(name, ", ")+4)
+salulation <- as.factor(salulation)
+
+library(dplyr)
+cabin1 <- as.factor(cabin1)
+dat1<- dat1 %>% mutate(cabin1, cabin2, salulation)
+
+dat1<- dat1 %>% select(-Name, -Cabin, -Sex)
+
+#library(infotheo)
+#dat1_Num_binned <- discretize(X=dat1[,c("Age","Fare")],disc="equalfreq", nbins=3)
+#dat2 <- data.frame(dat1[,-c(4,7)], dat1_Num_binned)
+
+set.seed(25)
+rows<-seq(1,nrow(dat1),1)
+train<-sample(rows,nrow(dat1)*.7)
+trainset<-dat1[train,]
+testset<-dat1[-train,]
 
 library(C50)
 
@@ -110,6 +135,12 @@ DT_C50 <- C5.0(Survived~.,data=trainset,rules=T)
 DT_C50
 summary(DT_C50)
 C5imp(DT_C50, pct=TRUE)
+
+trainset$pred <- predict(DT_C50, newdata = trainset)
+table(trainset$Survived, trainset$pred)
+
+testset$pred <- predict(DT_C50, newdata = testset)
+table(testset$Survived, testset$pred)
 
 ###################dplyr & ggplot2################
 library(dplyr)
