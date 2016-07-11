5d4897048e26efce38b19890ba135cb8aeb16fa8
NishanthGade <nishanth_gg@yahoo.com>
2016-07-11 21:35:54 +0530

    Update OfficePractice.R

--- OfficePractice.R
+++ OfficePractice.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 
-dat <- read.csv("E:\\INSOFE\\Project\\Job Recommendation\\Dataset\\TrainData.csv")
+dat <- read.csv("D:\\INSOFE\\Job recommendation engine\\TrainData.csv")
 
 str(dat)
 sum(is.na(dat))
@@ -57,7 +57,7 @@ moreThan20 <- ratingCount %>% filter(NoOfRating>=20) %>% arrange(desc(NoOfRating
 # Convert dataframe to matrix form
 mat <- as.matrix(datmat)
 
-mat2 <- mat
+mat2 <- mat[,-1]
 
 # Replace missing values with 0s
 mat2[which(is.na(mat2))] <- 0
@@ -72,13 +72,17 @@ e <- s$d
 e_sqare_energy = (e/sum(e))*100
 
 # Get the cumulative sum of varainces for each eigen value
-cumsum(e_sqare_energy)[1:500]
+cumsum(e_sqare_energy)[1:2021]
 
 # Get the user space matrix with top n eigen vectors
-user <- data.frame(s$u[,1:2])
+user <- data.frame(s$u[,1:2021])
+rownames(user) <- rownames(datmat)
+
+# Get the job space matrix with top n eigen vectors
+jobmat <- data.frame(s$v[,1:2021])
+rownames(jobmat) <- names(datmat[,-1])
 
 # Find cosine similarity of all users
-userCosineValues <- data.frame()
 for (i in 1:nrow(user))
 {
   for(j in 1:nrow(user))
@@ -87,9 +91,97 @@ for (i in 1:nrow(user))
   }
 }
 
-rownames(userCosineValues) <- rownames(datmat)
-names(userCosineValues) <- rownames(datmat)
+userCosineValues <- c()
+getCosineSimilarityOfUsers <- function(Applicant,ratedUsers) 
+{
+  userCosineValues <- c()
+  for(i in 1:nrow(ratedUsers))
+  {
+    if(ratedUsers$ApplicantID[i] != Applicant)
+    {
+      userCosineValues[i] <- getCosine(user[rownames(user)==Applicant,], user[rownames(user)==ratedUsers$ApplicantID[i],])     
+    }
 
+  }
+  return(userCosineValues)
+}
+
+# getCosineSimilarityOfJobs <- function(Job,ratedJobs) 
+# {
+#   userCosineValues <- c()
+#   for(i in 1:nrow(ratedJobs))
+#   {
+#     if(rownames(ratedJobs[i,]) != Job)
+#     {
+#       userCosineValues[i] <- getCosine(jobmat[rownames(jobmat)==Job,], jobmat[rownames(jobmat)==rownames(ratedJobs[i,]),])     
+#     }
+#     
+#   }
+#   return(userCosineValues)
+# }
+
+# Function to get average rating for a job
+# getAverageRating <- function(job)
+# {
+#   # Find 10 most similar jobs
+#   similarJobs <- getCosineSimilarityOfJobs(job, jobmat[rownames(jobmat)!=job, ])
+#   return(similarJobs)
+# }
+
+# Get the master data frame with User, Job, Rating and no NAs
+master <- datmat %>% melt(id=c("ApplicantID"), na.rm = T)
+names(master) <- c("ApplicantID", "JobID", "Rating")
+
+# Function to predict user rating
+predictRating <- function(Applicant, Job)
+{
+  # Get all the users that gave rating for this job
+  ratedUsers <- master %>% filter(JobID==Job & ApplicantID!=Applicant)
+  
+  if(nrow(ratedUsers) > 0)
+  {
+    # Get the cosine similarity scores of this Applicant with all other users who rated this job
+    ratedUsers$UserCorrelations <- getCosineSimilarityOfUsers(Applicant, ratedUsers)
+  
+    # Get the Pearson's coefficient
+    alpha <- 1/sum(abs(ratedUsers$UserCorrelations))
+  
+    # Get the predicted rating of 'Applicant' for JobID 'Job'
+    PredictedRating <- alpha * sum(ratedUsers$Rating * ratedUsers$UserCorrelations)
+  }
+  else
+    PredictedRating <- mean(master[master$ApplicantID==Applicant,3])
+  return(PredictedRating)
+  #return(ratedUsers)
+}
+
+
+
+
+
+# Get the predicted ratings of all applicants for jobs which they have given ratings
+for (i in 1:nrow(master))
+{
+  master$PredictedRating[i] <- predictRating(as.numeric(master[i,1]), as.numeric(master[i,2]))
+}
+
+master <- master[!is.na(master$PredictedRating),]
+
+#master$PredictedRating <- apply(master[,1], 1, predictRating, master[,2])
+#master$PredictedRating <- lapply(master[,1], predictRating, master[,2])
+
+# Evaluate the model for prediction
+RMSE <- sqrt(sum((master$Rating-master$PredictedRating)*(master$Rating-master$PredictedRating))/nrow(master))
+Accuracy <- nrow(master[abs(master$Rating-master$PredictedRating) < 0.75,])/nrow(master)
+
+r <- predictRating(11314, 4073)
+
+ru[ru$ApplicantID==9204 & ru$variable==456,]
+master[master$ApplicantID==9204 & master$JobID==456,]
+View(ru[1:10, 1:10])
+
+
+View(datmat[1:10, 1:10])
 View(userCosineValues[1:10, 1:10])
 
 user42 <- datmat[1,]
